1f723e89d01688448f00395dbcb442353f43a4f7
Kooditehdas <vesakoskela@gmail.com>
2015-04-09 22:01:36 +0300

    no message

---
+++ src/KT/KTTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace KT;
+
+class Carbon extends DateTime
+{
+	public function __construct()
+    {
+		
+	}
+	
+	public function Test()
+    {        
+        return 'Hello world';
+    }
+}
+
+
+?>
