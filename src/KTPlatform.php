3b9c2aebb4d219616d34cd898c996b204dc701d5
Kooditehdas <vesakoskela@gmail.com>
2015-04-12 00:14:58 +0300

    no message

--- src/KTPlatform.php
+++ src/KTPlatform.php
@@ -6,36 +6,48 @@ class KTPlatform
 {
 	static function toString()
 	{
+		include_once dirname(__FILE__) . '/../lib/Mobile_Detect.php';
+		
 		$md = new \Mobile_Detect;
 		return ($md->isMobile() ? 'mobile' : 'desktop');
 	}
 	
 	static function isMobile()
 	{
+		include_once dirname(__FILE__) . '/../lib/Mobile_Detect.php';
+		
 		$md = new \Mobile_Detect;
 		return ($md->isMobile() ? true : false);
 	}
 	
 	static function isTablet()
 	{
+		include_once dirname(__FILE__) . '/../lib/Mobile_Detect.php';
+		
 		$md = new \Mobile_Detect;
 		return ($md->isTablet() ? true : false);
 	}
 	
 	static function isDesktop()
 	{
+		include_once dirname(__FILE__) . '/../lib/Mobile_Detect.php';
+		
 		$md = new \Mobile_Detect;
 		return ($md->isMobile() ? false : true);
 	}
 	
 	static function isAndroidOS()
 	{
+		include_once dirname(__FILE__) . '/../lib/Mobile_Detect.php';
+		
 		$md = new \Mobile_Detect;
 		return ($md->isAndroidOS() ? true : false);
 	}
 	
 	static function isiOS()
 	{
+		include_once dirname(__FILE__) . '/../lib/Mobile_Detect.php';
+		
 		$md = new \Mobile_Detect;
 		return ($md->isiOS() ? true : false);
 	}	
