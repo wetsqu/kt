de8beb1df67f1566fe7afbd07bd1f9f3db718c37
Kooditehdas <vesakoskela@gmail.com>
2015-04-11 22:24:34 +0300

    no message

--- src/KTMinify.php
+++ src/KTMinify.php
@@ -4,9 +4,14 @@ namespace KT;
 
 class KTMinify
 {
+	function __construct()
+	{		
+	}
+	
 	static function js($files, $cached = false)
 	{	
-		$content = KTJSMin::js_content($files);
+		$minify = new KTMinify();		
+		$content = $minify->build($files, 'js');
 		
 		$accept_encoding_gzip = strpos(Request::capture()->header('Accept-Encoding'), 'gzip') !== false;
 		if ($accept_encoding_gzip) $content = gzencode($content, 5);
@@ -27,11 +32,35 @@ class KTMinify
 		return $response;
 	}
 	
-	static function js_content($files)
+	static function css($files, $cached = false)
+	{	
+		$minify = new KTMinify();		
+		$content = $minify->build($files, 'css');
+		
+		$accept_encoding_gzip = strpos(Request::capture()->header('Accept-Encoding'), 'gzip') !== false;
+		if ($accept_encoding_gzip) $content = gzencode($content, 5);
+		
+		$response = response($content, 200)->header('Content-Type', 'text/css');
+		
+		if ($cached)
+		{
+			$datetime_current = date('r', time());
+			$datetime_expires = date('r', time() + 3600 * 24 * 7);
+		
+			$response->setLastModified(new \DateTime($datetime_current))
+				     ->setExpires(new \DateTime($datetime_expires))
+				     ->setPublic();
+		}
+		
+		if ($accept_encoding_gzip) $response->header('Content-Encoding', 'gzip');
+		return $response;
+	}
+	
+	private function build($files, $dir)
 	{	
 		if (count($files) > 0)
 		{
-			$pc = new KTClosure();
+			$closure = new KTClosure();
 			$etag = '';
 			$content = '';
 			$filename = '';
@@ -41,7 +70,7 @@ class KTMinify
 				if (env('MINIFY', false))
 				{
 					// $pc->add($file)->simpleMode()->quiet()->hideDebugInfo();
-					$pc->add($file)->simpleMode()->quiet();
+					$closure->add($file)->simpleMode()->quiet();
 					$filename .= (strlen($filename) > 0 ? '-' : '') . basename($file);					
 					$etag .= md5(filemtime($file));
 				}
@@ -56,16 +85,16 @@ class KTMinify
 				$etag = md5($etag);				
 				$filename = str_replace('.', "_", $filename);
 				
-				if (Storage::exists('minify/js/' . $filename . '-' . $etag))
+				if (Storage::exists('ktminify/' . $dir . '/' . $filename . '-' . $etag))
 				{
-					return Storage::get('minify/js/' . $filename . '-' . $etag);
+					return Storage::get('ktminify/' . $dir . '/' . $filename . '-' . $etag);
 				}
 				else
 				{				
-					$content = $pc->compile();
+					$content = $closure->compile();
 					$content = pack("CCC",0xef,0xbb,0xbf) . $content;
 					
-					Storage::put('minify/js/' . $filename . '-' . $etag, $content);
+					Storage::put('ktminify/' . $dir . '/' . $filename . '-' . $etag, $content);
 					return $content;
 				}
 			}
