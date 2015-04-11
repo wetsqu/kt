aa25e96abc012b288073812d4fb4c17222e872d6
Kooditehdas <vesakoskela@gmail.com>
2015-04-12 00:03:39 +0300

    no message

--- src/KTPlatform.php
+++ src/KTPlatform.php
@@ -4,9 +4,40 @@ namespace KT;
 
 class KTPlatform
 {
-	function __construct()
+	static function toString()
 	{
-		
+		$md = new \Mobile_Detect;
+		return ($md->isMobile() ? 'mobile' : 'desktop');
+	}
+	
+	static function isMobile()
+	{
+		$md = new \Mobile_Detect;
+		return ($md->isMobile() ? true : false);
+	}
+	
+	static function isTablet()
+	{
+		$md = new \Mobile_Detect;
+		return ($md->isTablet() ? true : false);
+	}
+	
+	static function isDesktop()
+	{
+		$md = new \Mobile_Detect;
+		return ($md->isMobile() ? false : true);
+	}
+	
+	static function isAndroidOS()
+	{
+		$md = new \Mobile_Detect;
+		return ($md->isAndroidOS() ? true : false);
+	}
+	
+	static function isiOS()
+	{
+		$md = new \Mobile_Detect;
+		return ($md->isiOS() ? true : false);
 	}	
 }
 
