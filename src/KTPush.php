3906095557bb892a494681b4e385f3378875e272
Kooditehdas <vesakoskela@gmail.com>
2015-04-16 21:24:48 +0300

    no message

--- src/KTPush.php
+++ src/KTPush.php
@@ -5,16 +5,23 @@ namespace KT;
 class KTPush
 {
 	private $host = '';
+	private $app_env = '';
 	
-	function __construct($host)
+	/*function __construct($host, $app_env)
 	{
 		$this->host = $host;
-	}	
+	}*/
+	
+	function __construct($host, $app_env = '')
+	{
+		$this->host = $host;
+		$this->app_env = $app_env;
+	}
 	
 	public function send($to, $msg, $par)
 	{
 		$ktcurl = new KTCurl();
-		$ktcurl->rawPost('http://' . $this->host . '/pub' . '?id=' . $to, addslashes(json_encode(array('msg' => $msg, 'par' => $par ))));
+		$ktcurl->rawPost('http://' . $this->host . '/pub' . '?id=' . $to . '_' . $this->app_env, addslashes(json_encode(array('msg' => $msg, 'par' => $par ))));
 	}
 }
 	
